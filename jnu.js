ff98daee5572ff509eaca893985c59a3c38a0d5e
mmmtobezip <mmmszip@gmail.com>
2022-06-14 13:09:46 +0900

    renewal file

--- jnu.js
+++
@@ -1,8 +0,0 @@
-<select onchange="categoryChange(this)">
-    <option>제주대학교</option>
-    <option value="a">본부</option>
-    <option value="b">대학</option>
-    <option value="c">대학원</option>
-    <option value="d">부속시설</option>
-    <option value="e">산학협력단</option>
-</select>
