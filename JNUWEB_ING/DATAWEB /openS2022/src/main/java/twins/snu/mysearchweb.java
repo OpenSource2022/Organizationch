7b9fed81679fd04df2b7487571f523ab0e1bf4a6
mmmtobezip <mmmszip@gmail.com>
2022-06-15 00:10:12 +0900

    modify css & html

---
+++ JNUWEB_ING/DATAWEB /openS2022/src/main/java/twins/snu/mysearchweb.java
@@ -0,0 +1,95 @@
+package twins.snu;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+
+/**
+ * Servlet implementation class mysearchweb
+ */
+public class mysearchweb extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public mysearchweb() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
+		String n1 = request.getParameter("n1");
+		System.out.println("In Get " + n1);
+		/*
+		request.setAttribute("result", "<h2>" + n1 + n1 + "</h2>");
+		response.setContentType("text/html;charset=UTF-8");
+		getServletContext().getRequestDispatcher("/Retrieve.jsp").forward(request, response);
+		//).getWriter().append("Served at: ").append(request.getContextPath());
+		 * 
+		 */
+		String html = null;
+		try {
+			html = DoSQL(n1);
+			
+		} catch(Exception e) {
+			System.out.println("Error ");
+			e.printStackTrace();
+		}
+	//request.setAttribute("result", "<h2>" + n1 + n1 + "</h2>");
+		request.setAttribute("result",  html);
+		response.setContentType("text/html;charset=UTF-8");
+		getServletContext().getRequestDispatcher("/Retrieve.jsp").forward(request, response);
+	}
+	
+	String DoSQL(String p) throws Exception {
+		//String dburl = "jdbc:mysql://192.168.35.22:3306/jsps?useSSL=fasle";
+		String dburl = "jdbc:mysql://localhost:3306/jsps?useSSL=false&allowPublicKeyRetrieval=true";
+		String dbUser = "mmmszip", dbpwd="wlgus1086!!";
+		String sql = "select * from Jihyun where work like '%"+p+"%';";
+		System.out.println(sql);
+	Class.forName("com.mysql.cj.jdbc.Driver");
+	System.out.println("Driverfound");
+	Connection conn = DriverManager.getConnection(dburl, dbUser, dbpwd);
+	System.out.println("Driverconnect");
+
+	Statement stmt = conn.createStatement();
+	System.out.println("Driverstatement");
+	ResultSet rs = stmt.executeQuery(sql);
+	System.out.println("Driverexcute");
+	String rr = "<table>";
+	while(rs.next()) {
+		rr += "<tr>";
+		for(int i = 1; i <= 5; i++) {
+			rr += "<td>" + rs.getString(i) + "</td>";
+		}
+		rr += "</tr>";
+	}
+	rr += "</table>";
+	return (rr);
+}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ JNUWEB_ING/DATAWEB /openS2022/src/main/webapp/Entry.jsp
@@ -0,0 +1,33 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<link rel="stylesheet" type="text/css" href="Style.css" />
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet" />
+<!--  
+<link rel="stylesheet" type="text/css" href="Style.css" /> -->
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width">
+<title>OpenSourceWebProject</title>
+</head>
+<header><center><h1>JNU Search</h1></center></header>
+<body>
+<!--  <img src = "https://user-images.githubusercontent.com/617745575/95163201-34411c00-075c-11eb-9987-d6301acb4dab.png" class="weggle"/>
+-->
+<form method="post" action="mysearchweb">
+<div class ="box">
+	<div class="container-1">
+	<input type="text" id="text" name="n1" placeholder="검색어 입력">
+	<button class="icon" type="submit" name="click" value=""><i class="fa fa-search"></i></button>	
+	<!-- 	
+	<input type="text" id="text" name="n1" placeholder="검색어 입력" style="text-align:center; vertical-align:middle">
+		<span class="icon"><i class ="fa fa-search"></i></span>
+		<input type="text" id="text" placeholder="Search" />
+		<input type="submit" value="run">
+	 -->	 
+	</div>
+</div>
+</form>
+</body>
+</html>
